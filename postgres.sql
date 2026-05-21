be66fa17b66f6e2f6c0c424ffe2a3b80d0d5fb56
maxxxsudb <77297275+maxxxsudb@users.noreply.github.com>
2026-05-21 19:22:46 +0300

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -6,3 +6,14 @@ SELECT
 FROM pg_stat_user_tables
 ORDER BY dead_tuples DESC
 LIMIT 10;
+
+
+
+SELECT
+    relname AS table_name,
+    pg_size_pretty(pg_total_relation_size(relid)) AS total_size,
+    pg_size_pretty(pg_relation_size(relid)) AS table_size,
+    pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) AS index_size
+FROM pg_catalog.pg_statio_user_tables
+ORDER BY pg_total_relation_size(relid) DESC
+LIMIT 10;
